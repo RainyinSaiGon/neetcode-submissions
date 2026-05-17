c820ed435e39cb2cd5896588a77a39454b8c86f6
DINH DAI VU <161788675+RainyinSaiGon@users.noreply.github.com>
2026-05-17 18:43:30 +0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,31 @@
+class MinStack {
+
+    Deque <Integer> mainStack;
+    Deque <Integer> minStack;
+
+    
+    public MinStack() {
+        mainStack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek())
+            minStack.push(val);
+    }
+    
+    public void pop() {
+        int removeNum = mainStack.pop();
+        if (removeNum == minStack.peek())
+            minStack.pop();
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
